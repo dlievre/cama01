3741a0d07ab4461f2e8a5838fe46379697a0bd8e
dlievre <42@photeam.com>
2017-02-27 18:30:00 +0100

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,32 @@
+<?php
+require_once('includes_session.php');
+
+if ($_SESSION['valide'] == 'ok')
+{
+  $session = new CSession();
+  require_once('head.php');
+  require_once('header.php');
+  print('<div id="main">');
+
+  $CSession = new CSession();
+  $TabProfil = $CSession->get_profile();
+
+  $CPrint = new CPrint();
+  //$print->profil('Profile Utilisateur', $TabProfil);
+
+  $CPrint->titre('Profile Utilisateur');
+  //$CPrint->content_array($TabProfil, 'content_left', 'content_left');
+  //$CPrint->titre('Session Utilisateur');
+  $CPrint->content_array($_SESSION, 'content_left', 'content_left');
+  $CPrint->content( 'Connecté depuis '.round((time() - $_SESSION['Logstart'])/60 ). ' minutes', 'content_left', 'content_left'); // -time()
+//$CView->content( strval(time() - intval($_SESSION('Logstart'))), 'content_left', 'content_left'); // -time()
+
+
+  print('</div>'); 
+  include ('footer.php');
+}
+  else
+{
+  header('Location: index.php');
+}
+?>
